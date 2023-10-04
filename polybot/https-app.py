32f0d14849714a733799819130620766593de0c6
orrmb <126108038+orrmb@users.noreply.github.com>
2023-10-04 12:44:59 +0300

    Update https-app.py

--- polybot/https-app.py
+++ polybot/https-app.py
@@ -1,7 +1,3 @@
-WEBHOOK_LISTEN = '0.0.0.0'
-WEBHOOK_URL_BASE = "https://%s:%s" % (TELEGRAM_APP_URL, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (TELEGRAM_TOKEN)
-
 import telebot
 import flask
 from flask import request
@@ -12,16 +8,12 @@ import requests
 
 with open('/run/secrets/telegram_token', 'r') as file:
     TELEGRAM_TOKEN = file.read().strip()
-#TELEGRAM_APP_URL= 'https://97b7-34-217-129-240.ngrok-free.app/'
-TELEGRAM_APP_URL = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4').text
+
+HOST_IP = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4').text
 WEBHOOK_SSL_CERT='./certificates/certificate.pem'
 WEBHOOK_SSL_PRIV='./certificates/private-key.pem'
-WEBHOOK_LISTEN = '0.0.0.0'
-WEBHOOK_PORT= 8443
-WEBHOOK_LISTEN = '0.0.0.0'
-WEBHOOK_URL_BASE = "https://%s:%s" % (TELEGRAM_APP_URL, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (TELEGRAM_TOKEN
-
+TELEGRAM_APP_URL = "https://%s:8443" % (HOST_IP)
+WEBHOOK_URL_PATH = "/%s/" % (TELEGRAM_TOKEN)
 
 app = flask.Flask(__name__)
 
@@ -42,7 +34,7 @@ if __name__ == "__main__":
 
     bot = ObjectDetectionBot(TELEGRAM_TOKEN,WEBHOOK_URL_BASE, WEBHOOK_SSL_CERT)
     # Flask with https
-    app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT,
+    app.run(host='0.0.0.0', port=8443,
        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
        debug=False)
 
